ecbe12d5246dfd3af3bb1f88b9fd8b5d5ca3a5a3
Shika Shyam <91291183+shshyam147@users.noreply.github.com>
2022-05-02 11:32:28 -0400

    Updating Cleaning step to airflow DAG

--- src/airflow scripts/CleanData.py
+++ src/airflow scripts/CleanData.py
@@ -1,4 +1,27 @@
-import pandas as pd
+import googleapiclient.discovery
+from oauth2client.client import GoogleCredentials
+from google.oauth2 import service_account
+
 
 def CleanData():
-    print('Airflow job completed')
+    print('Extract job completed. Triggering Dataflow job')
+    credentials = service_account.Credentials.from_service_account_file('/home/airflow/gcs/data/mykey.json',)
+    dataflow = googleapiclient.discovery.build('dataflow', 'v1b3', credentials=credentials)
+    project = 'iconic-nimbus-348523'
+    location = 'us-east1'
+
+    result = dataflow.projects().locations().templates().launch(
+            projectId=project,
+            location = location,
+            body={
+            "environment": {
+                "zone": "us-east1-c",
+                "tempLocation": "gs://beampipeline1/temp/"
+            },
+            "jobName": 'beamfromoutside'
+            },
+            gcsPath = 'gs://beampipeline1/MyTemplate'
+    ).execute()
+
+    print('Job Triggered : '+ str(result)
+
