8da9b97cf564a87baf1566205bd29c7353fa35ba
Shika Shyam <91291183+shshyam147@users.noreply.github.com>
2022-05-01 23:25:47 -0400

    Updates to Beam script

--- src/working code/beam_pipeline.py
+++ src/working code/beam_pipeline.py
@@ -4,11 +4,35 @@ import argparse
 from apache_beam.options.pipeline_options import PipelineOptions
 from sys import argv
 import re
-from cleantext import clean
+
 
 PROJECT_ID = 'iconic-nimbus-348523'
 SCHEMA = 'text:STRING,created_at:STRING,tweet_id:STRING,location:STRING,user_screen_name:STRING,source:STRING,retweet_count:INTEGER,like_count:INTEGER,reply_count:INTEGER'
 table_spec = 'iconic-nimbus-348523:twitterstreamingdata.streamingdataset'
+
+
+def remove_emojis(data):
+    emoj = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642" 
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+        "]+", re.UNICODE)
+    return re.sub(emoj, '', data)
 
 def convert_types(data):
     """Converts string values to their appropriate type."""
@@ -32,7 +56,7 @@ def clean_tweets(data):
     data['text'] = re.sub(r"http\S+", "", data['text'])
     data['text'] = re.sub(r"www.\S+", "", data['text'])
     data['text'] = re.sub('[():#@]', ' ', data['text'])
-    data['text']=clean(data['text'], no_emoji=True)
+    data['text'] = remove_emojis(data['text'], no_emoji=True)
     return data
 
 
