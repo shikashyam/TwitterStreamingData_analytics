9b6d36fb3868c2c50b3425563a5e5dc970209189
Shika Shyam <91291183+shshyam147@users.noreply.github.com>
2022-05-02 10:13:13 -0400

    Updates to Beam script

--- src/working code/beam_pipeline.py
+++ src/working code/beam_pipeline.py
@@ -65,7 +65,8 @@ if __name__ == '__main__':
 
     p = beam.Pipeline(options=PipelineOptions())
 
-    (p | 'ReadTable' >> beam.io.ReadFromBigQuery(table=table_spec)
+    (p | 'ReadTable' >> beam.io.ReadFromBigQuery(query='SELECT * FROM '\
+              '[iconic-nimbus-348523:twitterstreamingdata.streamingdataset] ORDER BY created_at DESC LIMIT 5000')
        | 'ChangeDataType' >> beam.Map(convert_types)
        | 'DeleteUnwantedData' >> beam.Map(del_unwanted_cols)
        | 'CleanUpTweets' >> beam.Map(clean_tweets)
